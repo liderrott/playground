1adb003fa61d7a52fd44031fb9414aad2fbf228f
liderrott <umutcan_3012@hotmail.com>
2024-11-15 15:50:44 +0000

    Nov 15, 2024, 6:50 PM

--- src/components/three/Scene.jsx
+++ src/components/three/Scene.jsx
@@ -48,7 +48,7 @@ const Model = () => {
             if (Math.abs(vertex.y - bottomY) <= tolerance) {
               const point = new THREE.Vector2(
                 vertex.x * scene.scale.x,
-                vertex.z * scene.scale.x
+                vertex.z * scene.scale.y
               );
               
               // Tekrarlayan noktaları filtrele
