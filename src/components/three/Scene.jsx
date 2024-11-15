e5a3cf98befb28de88c9e7804cee271a2f97446f
liderrott <umutcan_3012@hotmail.com>
2024-11-15 16:44:00 +0000

    Nov 15, 2024, 7:44 PM

--- src/components/three/Scene.jsx
+++ src/components/three/Scene.jsx
@@ -9,7 +9,7 @@ import { BoxHelper } from 'three';
 
 const Model = () => {
   const modelRef = useRef();
-  const groundBoxRef = useRef();
+  const [boundingBox, setBoundingBox] = useState(null);
 
   const dracoLoader = new DRACOLoader();
   dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.6/');
@@ -20,7 +20,6 @@ const Model = () => {
 
   useEffect(() => {
     if (gltf.scene && modelRef.current) {
-      // Yeni scale değeri: 0.1
       gltf.scene.scale.set(0.1, 0.1, 0.1);
       
       // Normal bounding box
@@ -31,49 +30,32 @@ const Model = () => {
 
       // Boyutları hesapla
       const bbox = new THREE.Box3().setFromObject(gltf.scene);
-      const size = new THREE.Vector3();
-      bbox.getSize(size);
-      
-      // Ground box'ın boyutlarını güncelle
-      if (groundBoxRef.current) {
-        groundBoxRef.current.scale.set(
-          size.x * 0.1,
-          size.z * 0.1,
-          1
-        );
-        groundBoxRef.current.position.set(
-          (bbox.max.x + bbox.min.x) * 0.1 / 2,
-          0.01,
-          (bbox.max.z + bbox.min.z) * 0.1 / 2
-        );
-      }
-      
-      // Debug bilgileri
-      console.log('Model boyutları:', {
-        width: size.x * 0.1,
-        height: size.y * 0.1,
-        depth: size.z * 0.1
-      });
+      setBoundingBox(bbox);
     }
   }, [gltf]);
 
   return (
     <group ref={modelRef}>
       <primitive object={gltf.scene} />
-      
-      {/* Zemine yansıyan box */}
-      <mesh 
-        rotation={[-Math.PI / 2, 0, 0]}
-        ref={groundBoxRef}
-      >
-        <planeGeometry args={[1, 1]} />
-        <meshBasicMaterial 
-          color="#0000ff"
-          transparent
-          opacity={0.2}
-          side={THREE.DoubleSide}
-        />
-      </mesh>
+      {boundingBox && (
+        <mesh 
+          position={[0, 0.01, 0]}
+          rotation={[-Math.PI / 2, 0, 0]}
+        >
+          <planeGeometry 
+            args={[
+              (boundingBox.max.x - boundingBox.min.x) * 0.1,
+              (boundingBox.max.z - boundingBox.min.z) * 0.1
+            ]} 
+          />
+          <meshBasicMaterial 
+            color="#0000ff"
+            opacity={0.2}
+            transparent
+            side={THREE.DoubleSide}
+          />
+        </mesh>
+      )}
     </group>
   );
 };
@@ -83,7 +65,7 @@ const Scene = () => {
     <div style={{ height: 'calc(100vh - 64px)' }}>
       <Canvas 
         camera={{ 
-          position: [8, 8, 8], // Kamerayı biraz uzaklaştırdık
+          position: [8, 8, 8],
           fov: 50,
           near: 0.1,
           far: 1000
@@ -91,45 +73,29 @@ const Scene = () => {
         shadows
       >
         <Suspense fallback={null}>
-          {/* Işıklar */}
           <ambientLight intensity={0.5} />
           <directionalLight 
             position={[10, 10, 5]} 
             intensity={1}
             castShadow
-            shadow-mapSize-width={2048}
-            shadow-mapSize-height={2048}
           />
-
-          {/* Grid */}
           <Grid 
-            args={[20, 20]} // Grid'i büyüttük
+            args={[20, 20]}
             position={[0, 0, 0]}
             cellSize={0.5}
-            cellThickness={0.5}
             cellColor="#6f6f6f"
             sectionSize={2}
-            sectionThickness={1}
             sectionColor="#9d4b4b"
-            fadeDistance={30}
-            fadeStrength={1}
-            followCamera={false}
           />
-          
-          {/* Kontroller */}
           <OrbitControls 
             enableDamping={true}
             dampingFactor={0.05}
-            minDistance={4} // Min mesafeyi artırdık
-            maxDistance={20} // Max mesafeyi artırdık
+            minDistance={4}
+            maxDistance={20}
             maxPolarAngle={Math.PI / 2}
           />
-          
-          {/* Model */}
           <Model />
-          
-          {/* Yardımcı eksenler */}
-          <axesHelper args={[10]} /> {/* Eksenleri uzattık */}
+          <axesHelper args={[10]} />
         </Suspense>
       </Canvas>
     </div>
