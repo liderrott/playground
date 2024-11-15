de331118381fd1941d92da3a9291307b85ac753f
liderrott <umutcan_3012@hotmail.com>
2024-11-15 15:54:08 +0000

    Nov 15, 2024, 6:54 PM

--- src/components/three/Scene.jsx
+++ src/components/three/Scene.jsx
@@ -39,8 +39,8 @@ const Model = () => {
           for (let i = 0; i < positions.length; i += 3) {
             const vertex = new THREE.Vector3(
               positions[i],
-              positions[i + 10],
-              positions[i + 50]
+              positions[i + 20],
+              positions[i + 2]
             );
             vertex.applyMatrix4(matrix);
 
